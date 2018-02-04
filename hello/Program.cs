00f091bf4c02db76cff7d710245fe236fce02e44
andrzej morawski <andrzej.morawski@g3m.pl>
2018-02-04 14:24:11 +0100

    pierwszy program hello table [args]

--- hello/Program.cs
+++ hello/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System;
 
 namespace hello
 {
@@ -10,6 +8,16 @@ namespace hello
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+
+            //snippet cw+2xTab
+            Console.WriteLine("Witaj, " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("witaj, " + item);
+            //}  
+
         }
     }
 }
