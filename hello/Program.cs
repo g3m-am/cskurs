e4f65fcc5bc0eda02c933ee92108bad385398d08
andrzej morawski <andrzej.morawski@g3m.pl>
2018-02-05 09:27:08 +0100

    lesson 5 parse

--- hello/Program.cs
+++ hello/Program.cs
@@ -8,10 +8,30 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            // string name = args[0];
 
             //snippet cw+2xTab
+            //Console.WriteLine("Witaj, " + name);
+            //Console.ReadKey();
+
+            Console.WriteLine("wpisz imie: ");
+            string name = Console.ReadLine();
             Console.WriteLine("Witaj, " + name);
+
+            Console.WriteLine("ile lat: ");
+            int age = int.Parse(Console.ReadLine());
+            
+            if (age > 18)
+            {
+                Console.WriteLine("jestes dorosly");
+            }
+            else
+            {
+                Console.WriteLine("za mlody");
+            }
+
+            Console.ReadKey();
+
 
             //foreach (var item in args)
             //{
