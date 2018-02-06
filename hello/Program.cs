74233ac3c003f3d02b32b76eb89d3edddfe19d32
andrzej morawski <andrzej.morawski@g3m.pl>
2018-02-06 07:40:46 +0100

    lesson 6 foreach

--- hello/Program.cs
+++ hello/Program.cs
@@ -8,29 +8,45 @@ namespace hello
     {
         static void Main(string[] args)
         {
+            for (;;)
+            {
+
+                Console.WriteLine("wpisz imie: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Witaj, " + name);
+
+                Console.WriteLine("ile lat: ");
+                //int age = int.Parse(Console.ReadLine());
+
+                int age;
+                bool result = int.TryParse(Console.ReadLine(), out age);
+
+                if (age > 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("jestes dorosly");
+                }
+                else
+                if (!result)
+                {
+                    Console.WriteLine("niepoprawna wartosc");
+                }
+                else
+                {
+                    Console.WriteLine("za mlody");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+                Console.ResetColor();
+            }
+
             // string name = args[0];
 
             //snippet cw+2xTab
             //Console.WriteLine("Witaj, " + name);
             //Console.ReadKey();
 
-            Console.WriteLine("wpisz imie: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Witaj, " + name);
-
-            Console.WriteLine("ile lat: ");
-            int age = int.Parse(Console.ReadLine());
-            
-            if (age > 18)
-            {
-                Console.WriteLine("jestes dorosly");
-            }
-            else
-            {
-                Console.WriteLine("za mlody");
-            }
-
-            Console.ReadKey();
 
 
             //foreach (var item in args)
