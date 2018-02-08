6d40122d61db66c56bb16de61c8e90af48d8a00d
andrzej morawski <andrzej.morawski@g3m.pl>
2018-02-08 13:52:00 +0100

    lesson 8 cd

--- StudentDiary/Program.cs
+++ StudentDiary/Program.cs
@@ -13,8 +13,44 @@ namespace StudentDiary
         static void Main(string[] args)
         {
             Diary diary = new Diary();
-            diary.AddRating(5);
-            diary.AddRating(8.5f);
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+
+            //float avg = diary.CalculateAverage();
+            //float max = diary.GiveMaxRating();
+            //float min = diary.GiveMinRating();
+
+            for (;;)
+            {
+                Console.WriteLine("podaj ocene ");
+
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
+
+                
+
+                if (rating == 11)
+                {
+                    break;
+                }
+
+                if (result)
+                    if (rating > 0 && rating <= 10)
+                    {
+                        diary.AddRating(rating);
+                    }
+                    else
+                    {
+                        Console.WriteLine("niepoprawna liczba");
+                    }
+            }
+
+
+            Console.WriteLine("srednia = " + diary.CalculateAverage());
+            Console.WriteLine("max = " + diary.GiveMaxRating());
+            Console.WriteLine("a min to = " + diary.GiveMinRating());
+
+            Console.ReadKey();
         }
     }
 
